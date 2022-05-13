ebfb1f3cdd9a25186b271b38bd3306c981ab38a9
andarpratama <andar.webdev@gmail.com>
2022-05-13 19:09:51 +0700

    install + setup mongoose and create product module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductsController } from './products/products.controller';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
+  imports: [
+    MongooseModule.forRoot('mongodb://localhost/nest-mini-onlineshop'),
+    ProductsModule,
+  ],
+  controllers: [AppController, ProductsController],
   providers: [AppService],
 })
 export class AppModule {}
